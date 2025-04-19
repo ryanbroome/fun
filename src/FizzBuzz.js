e55830dcf656ba4f371a4b2593f8bf83feb21b56
Ryan <110047631+ryanbroome@users.noreply.github.com>
2025-04-19 15:32:51 -0400

    Update FizzBuzz.js

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -11,3 +11,14 @@ function fizzbuzz(n) {
     }
   }
 }
+
+function fizzbuzzAI(n) {
+  for (let i = 1; i <= n; i++) {
+    let output = "";
+
+    if (i % 3 === 0) output += "Fizz";
+    if (i % 5 === 0) output += "Buzz";
+
+    console.log(output || i);
+  }
+}
